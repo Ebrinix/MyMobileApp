6deb5e660e8cefeba4a0d5c883d35e7043ea3b44
Ebrinix <121ebrinix@gmail.com>
2021-11-02 14:41:16 +0100

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(MyApp());
   runApp(new MaterialApp(
     home: new MyApp(),
   ));
@@ -12,195 +11,143 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 
-class _State  extends State<MyApp> {
-
-  // For Adding And Subtracting Number Start
-  // int _value= 0; 
-
-  // void _add() {
-  //   setState(() {
-  //     _value++;
-  //   });
-  // } 
-
-  // void _subtract() {
-  //   setState(() {
-  //     _value--;
-  //   });
-  // }
-  // For Adding And Subtracting Number End
-
-  String _value = '';
-  
-  void _onChange(String value) {
-    setState(() => _value = '${value}');
+class _State extends State<MyApp> {
+  void _showBottom() {
+    showModalBottomSheet<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return new Container(
+              padding: new EdgeInsets.all(15.0),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Text(
+                    'Some info here ',
+                    style: new TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold),
+                  ),
+                  new RaisedButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: new Text('Close'),
+                  )
+                ],
+              ));
+        });
   }
-  
-  // void _onSubmit(String value) {
-  //   setState(() => _value = 'Submit: ${value}');
-  // }
 
+  List<BottomNavigationBarItem> _items =
+      List<BottomNavigationBarItem>.empty(growable: true);
+  String _value = '';
+  int _index = 0;
+
+  @override
+  void initState() {
+    // _items = new List();
+    _items.add(new BottomNavigationBarItem(
+        icon: new Icon(Icons.people), title: new Text('People')));
+    _items.add(new BottomNavigationBarItem(
+        icon: new Icon(Icons.mobile_friendly), title: new Text('Phone')));
+    _items.add(new BottomNavigationBarItem(
+        icon: new Icon(Icons.message), title: new Text('Message')));
+    // _items.add(new BottomNavigationBarItem(
+    //     icon: new Icon(Icons.messenger), title: new Text('Messenger')));
+  }
+
+  TextEditingController _user = new TextEditingController();
+  TextEditingController _pass = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('New App'),
+        title: new Text('Name here'),
       ),
       body: new Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new Center(
-          child: new Column(
-            children: <Widget>[
-              // new Text(_value),
-              new TextField(
-                decoration: new InputDecoration(
-                  labelText: 'Hello',
-                  hintText: 'Hint',
-                  icon: new Icon(Icons.check)
+          padding: new EdgeInsets.all(22.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Text(_value),
+                new Text('Add Widget Here'),
+                new RaisedButton(
+                  onPressed: _showBottom,
+                  child: new Text('Click me'),
                 ),
-                autocorrect: true,
-                autofocus: false,
-                keyboardType: TextInputType.text,
-                onChanged: _onChange,
-                // onSubmitted: _onSubmit,
-              )
-              // For Adding And Subtracting Number Start
-              // new Text('Value = ${_value}'),
-              // new IconButton(icon: new Icon(Icons.add), onPressed: _add),
-              // new IconButton(icon: new Icon(Icons.remove), onPressed: _subtract)
-              // For Adding And Subtracting Number End
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        // When the user presses the button, show an alert dialog containing
-        // the text that the user has entered into the text field.
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                // Retrieve the text the that user has entered by using the
-                // TextEditingController.
-                content: Text(_value),
-                // content: Text('Hello World'),
-              );
-            },
-          );
+                Padding(
+                  padding: new EdgeInsets.all(12.0),
+                ),
+                new Text('Please Login'),
+                new Row(children: <Widget>[
+                  new Text('Username: '),
+                  new Expanded(
+                      child: TextField(
+                    controller: _user,
+                  ))
+                ]),
+                new Row(children: <Widget>[
+                  new Text('Password: '),
+                  new Expanded(
+                      child: TextField(
+                    controller: _pass,
+                    obscureText: true,
+                  ))
+                ]),
+                new Padding(
+                  padding: new EdgeInsets.all(32.0),
+                  child: new RaisedButton(
+                    onPressed: () => print('Login ${_user.text}'),
+                    child: new Text('Click me'),
+                  ),
+                ),
+                new Card(
+                  child: new Container(
+                    width: double.infinity,
+                    padding: new EdgeInsets.all(32.0),
+                    child: new Column(
+                      children: <Widget>[
+                        new Text('Hello World'),
+                        new Text('How are you?'),
+                      ],
+                    ),
+                  ),
+                ),
+                new Card(
+                  child: new Container(
+                    padding: new EdgeInsets.all(32.0),
+                    child: new Column(
+                      children: <Widget>[
+                        new Text('Hello World'),
+                        new Text('How are you?'),
+                      ],
+                    ),
+                  ),
+                ),
+                new Card(
+                  child: new Container(
+                    padding: new EdgeInsets.all(32.0),
+                    child: new Column(
+                      children: <Widget>[
+                        new Text('Hello World'),
+                        new Text('How are you?'),
+                      ],
+                    ),
+                  ),
+                ),
+                // new Column(new Text('Add widget here')),
+              ],
+            ),
+          )),
+      bottomNavigationBar: new BottomNavigationBar(
+        items: _items,
+        fixedColor: Colors.blue,
+        currentIndex: _index,
+        onTap: (int item) {
+          setState(() {
+            _index = item;
+            _value = "Current value is: ${_index.toString()}";
+          });
         },
-        tooltip: 'Show me the value!',
-        child: const Icon(Icons.earbuds),
       ),
     );
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // Try running your application with "flutter run". You'll see the
-//         // application has a blue toolbar. Then, without quitting the app, try
-//         // changing the primarySwatch below to Colors.green and then invoke
-//         // "hot reload" (press "r" in the console where you ran "flutter run",
-//         // or simply save your changes to "hot reload" in a Flutter IDE).
-//         // Notice that the counter didn't reset back to zero; the application
-//         // is not restarted.
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: MyHomePage(title: 'Flutter Demo Home qqq'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have roll times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.access_time_filled_outlined),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
